7b9ed7a120ae324718a9bc397e8f0f87999c3f3b
Han Frisonsova <hanafrisonsova@gmail.com>
2025-04-13 23:29:39 +0200

    upravi zamezeni prepisovani tlacitek a prida funkci na restart pro potvrzení

--- hra.js
+++ hra.js
@@ -3,27 +3,27 @@ let currentPlayer = 'circle';
 const buttonElements = document.querySelectorAll('button');
 
 buttonElements.forEach((button) => {
-  button.addEventListener('click', () => {
-    if (button.innerHTML !== '') return;
+  button.addEventListener('click', (event) => {
+    event.target.disabled = true;
     if (currentPlayer === 'circle') {
       button.innerHTML = ` 
-      <div class="board__field--circle">
-  <img
-    src="podklady/circle-black.svg"
-    alt="černý kruh"
-  />
-</div>`;
+        <div class="board__field--circle">
+        <img
+          src="podklady/circle-black.svg"
+          alt="černý kruh"
+        />
+        </div>`;
       currentPlayer = 'cross';
       const playerElement = document.querySelector('.hra__player');
       playerElement.innerHTML = `<img src="podklady/cross.svg" alt="bílý kříž" />`;
     } else {
       button.innerHTML = ` 
-      <div class="board__field--cross">
-      <img
-        src="podklady/cross-black.svg"
-        alt="černý kříž"
-      />
-    </div>`;
+        <div class="board__field--cross">
+        <img
+          src="podklady/cross-black.svg"
+          alt="černý kříž"
+        />
+        </div>`;
       currentPlayer = 'circle';
       const playerElement = document.querySelector('.hra__player');
       playerElement.innerHTML = `<img src="podklady/circle.svg" alt="bílý kruh" />`;
@@ -31,9 +31,13 @@ buttonElements.forEach((button) => {
   });
 });
 
+const restartElement = document.querySelector('.hra__restart');
+restartElement.addEventListener('click', (event) => {
+  if (window.confirm('Jste si jistí, že chcete hru restartovat?')) {
+    window.open('hra.html');
+  } else {
+    event.preventDefault();
+  }
+});
 /*
-Bonus
-Může se stát, že uživatel se omylem uklikne a modrým tlačítkem pro restart přijde o rozehranou hru. Proto přidej modrému odkazu posluchač události, který se po kliknutí uživatele zeptá zabudovanou funcí confirm, jestli chce hru opravdu restartovat. Pokud ne, zavolej event.preventDefault(), čímž zabráníš tomu, aby prohlížeč přešel na odkazovanou stránku (tj. načetl stránku s prázdnou hrou). Funkce confirm vrací true nebo false podle toho, zda uživatel souhlasil nebo nesouhlasil s potvrzovací zprávou v dialogu.
-ukázka restartu
-
 Pomocí animací přidej symbolům efekt postupného zvětšení. V náhledu je animace zpomalená z času 0,15 s na 0.45 s, aby bylo lépe vidět, jak má vypadat. V tvém kódu po otestování použij čas kratší, aby hra působila svižně. */
