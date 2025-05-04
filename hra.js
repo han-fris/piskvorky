28db22b444b940a18479cf0a4688769bd3b8d2a5
Han Frisonsova <hanafrisonsova@gmail.com>
2025-05-04 16:17:17 +0200

    vytvori pole po kazdem kliknuti s aktualnim stavem hry

--- hra.js
+++ hra.js
@@ -1,3 +1,5 @@
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
+
 let currentPlayer = 'circle';
 
 const buttonElements = document.querySelectorAll('button');
@@ -6,29 +8,33 @@ buttonElements.forEach((button) => {
   button.addEventListener('click', (event) => {
     event.target.disabled = true;
     if (currentPlayer === 'circle') {
+      button.classList.add('o');
       button.innerHTML = ` 
-        <div class="hra__image-container ">
+        <div class="hra__image-container">
         <img
           class="hra__showing"
           src="podklady/circle-black.svg"
           alt="černý kruh"
         />
         </div>`;
+      console.log(createArray());
       currentPlayer = 'cross';
       const playerElement = document.querySelector('.hra__player');
       playerElement.innerHTML = `<img src="podklady/cross.svg" alt="bílý kříž" />`;
     } else {
+      button.classList.add('x');
       button.innerHTML = ` 
-        <div class="hra__image-container">
+        <div class="hra__image-container x">
         <img
           class="hra__showing"
           src="podklady/cross-black.svg"
           alt="černý kříž"
         />
         </div>`;
+      console.log(createArray());
       currentPlayer = 'circle';
       const playerElement = document.querySelector('.hra__player');
-      playerElement.innerHTML = `<img src="podklady/circle.svg" alt="bílý kruh" />`;
+      playerElement.innerHTML = `<img src="podklady/circle.svg" alt="bílý kruh"/>`;
     }
   });
 });
@@ -42,3 +48,19 @@ restartElement.addEventListener('click', (event) => {
     event.preventDefault();
   }
 });
+
+//VYTVOŘIT POLE S ÚDAJI O AKTUÁLNÍM STAVU HRY
+function createArray() {
+  const array = [];
+  buttonElements.forEach((button) => {
+    if (button.classList.contains('x')) {
+      array.push('x');
+    } else if (button.classList.contains('o')) {
+      array.push('o');
+    } else {
+      array.push('_');
+    }
+  });
+
+  return array;
+}
